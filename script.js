92670c0cd5c4c4db7cbd70ecd948773d8bfc7c8a
nguyenquangnam1988vt-eng <nguyenquangnam1988vt@gmail.com>
2026-04-21 11:44:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const ctx = canvas.getContext("2d");
 
 // ==================== CẤU HÌNH ====================
 const PLATE_MODEL_PATH = "/model/bienso1.onnx";
-const CHAR_MODEL_PATH = "https://drive.google.com/uc?export=download&id=1HVBKhOucjjgfycmGp2c8Bl13WuD8neXI";
+const CHAR_MODEL_PATH = "/model/character.onnx";
 const INPUT_SIZE = 640;
 
 // Ngưỡng cho biển số
@@ -29,12 +29,10 @@ const FPS_LIMIT = 2;
 
 // Danh sách class ký tự
 const CHAR_CLASSES = {
-    0: '-', 
-    1: '0', 2: '1', 3: '2', 4: '3', 5: '4', 6: '5', 7: '6', 8: '7', 9: '8', 10: '9',
-    11: 'A', 12: 'B', 13: 'C', 14: 'D', 15: 'E', 16: 'F', 17: 'G',
-    18: 'H', 19: 'I', 20: 'J', 21: 'K', 22: 'L', 23: 'M', 24: 'N',
-    25: 'O', 26: 'P', 27: 'Q', 28: 'R', 29: 'S', 30: 'T',
-    31: 'U', 32: 'V', 33: 'W', 34: 'X', 35: 'Y', 36: 'Z'
+    0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+    10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G',
+    17: 'H', 18: 'K', 19: 'L', 20: 'M', 21: 'N', 22: 'P', 23: 'R',
+    24: 'S', 25: 'T', 26: 'U', 27: 'V', 28: 'X', 29: 'Y', 30: 'Z'
 };
 
 // ==================== Biến toàn cục ====================
@@ -492,8 +490,7 @@ async function detectCharactersOnCrop(plateCanvas) {
         const h = output.data[i + 3 * numBoxesTotal];
         let bestClass = -1;
         let bestScore = 0;
-       const NUM_CLASSES = Object.keys(CHAR_CLASSES).length;
-       for (let c = 0; c < NUM_CLASSES; c++) {
+        for (let c = 0; c < 31; c++) {
             const score = output.data[i + (4 + c) * numBoxesTotal];
             if (score > bestScore) {
                 bestScore = score;
